c96f4a0b08e3d509cec41470f6dd9345e8e4311b
Smart Vain <zlc.blue15210@gmail.com>
2022-08-13 17:02:10 +0900

    add: comment

--- nuxt-front/pages/dashboard.vue
+++ nuxt-front/pages/dashboard.vue
@@ -190,8 +190,8 @@ export default {
     return {
       bigLineChart: {
         allData: [
-          [0, 20, 10, 30, 15, 40, 20, 60, 60],
-          [0, 20, 5, 25, 10, 30, 15, 40, 40],
+          [0, 20, 10, 30, 15, 40, 20, 60, 60], // Month
+          [0, 20, 5, 25, 10, 30, 15, 40, 40], // Week
         ],
         activeIndex: 0,
         chartData: {
